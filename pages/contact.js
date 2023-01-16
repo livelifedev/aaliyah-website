f418fd9da8dec44c685beae8af06c0caaa6f78be
John <johnrubio93@gmail.com>
2023-01-16 23:44:12 +1100

    update form

--- pages/contact.js
+++ pages/contact.js
@@ -23,17 +23,32 @@ export default function Contact() {
             <p>Book a complimentary call</p>
           </div>
           <div className={styles.section}>
-            <form
-              action="mailto:aaliyah.r1990@gmail.com"
-              method="post"
-              enctype="text/plain"
-              autocomplete="off"
-            >
-              <input type="text" placeholder="ENTER FULL NAME" />
-              <input type="email" placeholder="ENTER EMAIL" />
-              <input type="text" placeholder="ENTER PHONE NUMBER" />
-              <input type="text" placeholder="PREFERRED CONTACT METHOD" />
-              <textarea rows="8" placeholder="MESSAGE" />
+            <form action="mailto:aaliyah.r1990@gmail.com" method="GET">
+              <input
+                type="text"
+                placeholder="ENTER FULL NAME"
+                name="subject"
+                required
+              />
+              <input
+                type="email"
+                placeholder="ENTER EMAIL"
+                name="email"
+                required
+              />
+              <input
+                type="text"
+                placeholder="ENTER PHONE NUMBER"
+                name="phone"
+                required
+              />
+              <input
+                type="text"
+                placeholder="PREFERRED CONTACT METHOD"
+                name="contact"
+                required
+              />
+              <textarea rows="8" placeholder="MESSAGE" name="body" required />
               <button type="submit">Submit</button>
             </form>
           </div>
