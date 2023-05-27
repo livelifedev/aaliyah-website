185bbdda07a7606678b3df78f412d43396aba572
John <johnrubio93@gmail.com>
2023-05-27 21:19:20 +1000

    add shop

--- pages/shop.js
+++ pages/shop.js
@@ -1,9 +1,23 @@
+import Head from "next/head";
 import { ProductBrowser } from "@ecwid/nextjs-ecwid-plugin";
+import styles from "../styles/Shop.module.css";
 
 export default function Store() {
   return (
     <>
-      <ProductBrowser storeId="87830761" />
+      <Head>
+        <title>Aaliyah Rubio | Services</title>
+        <meta
+          name="description"
+          content="Aaliyah Rubio - Holistic therapy positive psychology coach"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={styles.wrapper}>
+        <ProductBrowser storeId="87830761" />
+      </div>
     </>
   );
 }
